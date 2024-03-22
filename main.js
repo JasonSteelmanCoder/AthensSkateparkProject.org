2d209165d6e758fd7d1b3bbe496f3ad4aa489784
Jason <jason.steelman@sposatogse.org>
2024-03-21 21:58:48 -0400

    add alts to rotating gallery images

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 const galleryImage = document.getElementById('gallery-image');
 
 const galleryPhotos = {
-    0: './galleryPhotos/learning.jpg',
-    1: './galleryPhotos/roller.jpg',
-    2: './galleryPhotos/community.jpg',
-    3: './galleryPhotos/devikiyo.jpg',
-    4: './galleryPhotos/kf_indy.jpg',
-    5: './galleryPhotos/bmx.jpg',
+    0: {"src": './galleryPhotos/learning.jpg', "alt": "boy learning to skateboard"},
+    1: {"src": './galleryPhotos/roller.jpg', "alt": "female roller skater doing trick with long hair died pink"},
+    2: {"src": './galleryPhotos/community.jpg', "alt": "group of people laughing together, gathered on top of a ramp"},
+    3: {"src": './galleryPhotos/devikiyo.jpg', "alt": "man flipping a skateboard at the top of a ramp"},
+    4: {"src": './galleryPhotos/kf_indy.jpg', "alt": "man soaring above the skatepark flipping his skateboard under him"},
+    5: {"src": './galleryPhotos/bmx.jpg', "alt": "man precariously balancing on a bmx bike"},
 };
 
 let currentIndex = 0;
 
-galleryImage.setAttribute("src", galleryPhotos[currentIndex]);
+galleryImage.setAttribute("src", galleryPhotos[currentIndex]["src"]);
+galleryImage.setAttribute("alt", galleryPhotos[currentIndex]["alt"]);
 
 setInterval(() => {
     currentIndex += 1;
     currentIndex = currentIndex % Object.keys(galleryPhotos).length;
-    galleryImage.setAttribute('src', galleryPhotos[currentIndex]);
+    galleryImage.setAttribute('src', galleryPhotos[currentIndex]["src"]);
+    galleryImage.setAttribute('alt', galleryPhotos[currentIndex]["alt"]);
 }, 4000);
