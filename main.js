02967a0c0d7198f4a8c92d662c1974685238ed4f
Jason <jason.steelman@sposatogse.org>
2024-03-25 09:32:46 -0400

    update progress bar, organize images

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const galleryImage = document.getElementById('gallery-image');
 
 const galleryPhotos = {
-    0: {"src": './galleryPhotos/learning.jpg', "alt": "boy learning to skateboard"},
-    1: {"src": './galleryPhotos/roller.jpg', "alt": "female roller skater doing trick with long hair died pink"},
-    2: {"src": './galleryPhotos/community.jpg', "alt": "group of people laughing together, gathered on top of a ramp"},
-    3: {"src": './galleryPhotos/devikiyo.jpg', "alt": "man flipping a skateboard at the top of a ramp"},
-    4: {"src": './galleryPhotos/kf_indy.jpg', "alt": "man soaring above the skatepark flipping his skateboard under him"},
-    5: {"src": './galleryPhotos/bmx.jpg', "alt": "man precariously balancing on a bmx bike"},
+    0: {"src": './mainImages/galleryPhotos/learning.jpg', "alt": "boy learning to skateboard"},
+    1: {"src": './mainImages/galleryPhotos/roller.jpg', "alt": "female roller skater doing trick with long hair died pink"},
+    2: {"src": './mainImages/galleryPhotos/community.jpg', "alt": "group of people laughing together, gathered on top of a ramp"},
+    3: {"src": './mainImages/galleryPhotos/devikiyo.jpg', "alt": "man flipping a skateboard at the top of a ramp"},
+    4: {"src": './mainImages/galleryPhotos/kf_indy.jpg', "alt": "man soaring above the skatepark flipping his skateboard under him"},
+    5: {"src": './mainImages/galleryPhotos/bmx.jpg', "alt": "man precariously balancing on a bmx bike"},
 };
 
 let currentIndex = 0;
